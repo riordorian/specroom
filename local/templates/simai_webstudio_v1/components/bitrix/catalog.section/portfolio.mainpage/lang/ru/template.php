97e4265e79acccd8b8225532aab24339856a5814
riordorian <riordorian@mail.ru>
2018-03-24 22:32:34 +0300

    #base v3

--- local/templates/simai_webstudio_v1/components/bitrix/catalog.section/portfolio.mainpage/lang/ru/template.php
+++ local/templates/simai_webstudio_v1/components/bitrix/catalog.section/portfolio.mainpage/lang/ru/template.php
@@ -1,4 +1,4 @@
 <?
-$MESS["SOME_WORKS"] = "<strong>Наш сервис</strong>";
+$MESS["SOME_WORKS"] = "<strong>Наш сервисный центр</strong>";
 $MESS["CT_BCS_ELEMENT_DELETE_CONFIRM"] = "Будет удалена вся информация, связанная с этой записью. Продолжить?";
 ?>
