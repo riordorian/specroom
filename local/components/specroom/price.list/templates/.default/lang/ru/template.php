1892e8fd18bcfdecb5767139634fc6014f504468
riordorian <riordorian@mail.ru>
2018-04-05 17:45:32 +0300

    #price title bug fixing

--- local/components/specroom/price.list/templates/.default/lang/ru/template.php
+++ local/components/specroom/price.list/templates/.default/lang/ru/template.php
@@ -1,7 +1,7 @@
 <? $MESS['TITLE'] = 'Цены на ремонт';
 $MESS['SERVICE_NAME'] = 'Услуга';
 $MESS['SERVICE_NAME_2'] = 'Ремонт';
-$MESS['DURATION'] = 'Время ремонта';
+$MESS['DURATION'] = 'Сроки';
 $MESS['PRICE_DESKTOP'] = 'Цена (включает в себя работу мастера и стоимость деталей)';
 $MESS['PRICE_MOBILE'] = 'Цена (детали + работа)';
 $MESS['FULL_PRICE'] = 'Узнать все цены';
